6f9dbf49e38bf9de5c22fa5af6eaeb4b8d07f89d
Consciousnesszz <consciousness1@gmail.com>
2017-04-10 12:31:59 +0800

    2.0

--- backstage/js/config.js
+++ backstage/js/config.js
@@ -25,7 +25,33 @@ app.config(function($stateProvider, $urlRouterProvider){
   				templateUrl: '/html/client.html'
   			}
   		}
-	}).state("allProduct", {
+	}).state("VIPClient", {
+      url: '/VIPClient',
+      views:{
+        'nav':{
+          templateUrl: '/html/nav.html'
+        },
+        'aside':{
+          templateUrl: '/html/aside.html'
+        },
+        'article':{
+          templateUrl: '/html/client.html'
+        }
+      }
+  }).state("normalClient", {
+      url: '/normalClient',
+      views:{
+        'nav':{
+          templateUrl: '/html/nav.html'
+        },
+        'aside':{
+          templateUrl: '/html/aside.html'
+        },
+        'article':{
+          templateUrl: '/html/client.html'
+        }
+      }
+  }).state("allProduct", {
   		url: '/allProduct',
   		views:{
   			'nav':{
@@ -38,5 +64,44 @@ app.config(function($stateProvider, $urlRouterProvider){
   				templateUrl: '/html/product.html'
   			}
   		}
-	});;
+	}).state("saleProduct", {
+      url: '/saleProduct',
+      views:{
+        'nav':{
+          templateUrl: '/html/nav.html'
+        },
+        'aside':{
+          templateUrl: '/html/aside.html'
+        },
+        'article':{
+          templateUrl: '/html/product.html'
+        }
+      }
+  }).state("underProduct", {
+      url: '/underProduct',
+      views:{
+        'nav':{
+          templateUrl: '/html/nav.html'
+        },
+        'aside':{
+          templateUrl: '/html/aside.html'
+        },
+        'article':{
+          templateUrl: '/html/product.html'
+        }
+      }
+  }).state("detail", {
+      url: '/detail',
+      views:{
+        'nav':{
+          templateUrl: '/html/nav.html'
+        },
+        'aside':{
+          templateUrl: '/html/aside.html'
+        },
+        'article':{
+          templateUrl: '/html/detail.html'
+        }
+      }
+  });
 })
--- backstage/js/controller.js
+++ backstage/js/controller.js
@@ -1,7 +1,7 @@
-	/*---------login module---------*/
+	/*---------controller module---------*/
 var controller = angular.module('controllerModule', ['ngCookies', 'serviceModule']);
-controller.controller('loginCtrl', function($scope, $http, $cookies, userService){
 
+controller.controller('loginCtrl', function($scope, $http, $cookies, userService){
 	var username, password, keep;
 	var oldUser = $cookies.getObject('user');
 	if (oldUser) {
@@ -52,18 +52,145 @@ controller.controller('loginCtrl', function($scope, $http, $cookies, userService
 	}
 });
 
-controller.controller('navCtrl', function($scope){
+controller.controller('navCtrl', function($scope, $rootScope, $location, navService){
+	$scope.lookingFor = navService.lookingFor();
+	$('.seek .btn').click(function(){
+		var target = $(".seek .search").val(),
+			listItem = $(".list div");
+		for(var i = 0, len = listItem.length; i < len; i++){
+			$(listItem[i]).css("background", "#f1f1f1");
+			if ($(listItem[i]).text() == target) {
+				$(listItem[i]).css("background", "#ff0");
+				continue;
+			}
+		}
+	})
+	$rootScope.onChange = function(){
+		var statu = $location.path();
+		if (statu.indexOf("lient") > 0) {
+			$rootScope.clients.reverse();
+		}else{
+			$rootScope.products.reverse();
+		}
+	}
+	$(".add .btn").click(function(){
+		$(".wadd").css("display", "block");
+	})
+});
+
+controller.controller('asideCtrl', function($scope, $cookies){
+	var username = $cookies.getObject('user').username;
+	$scope.username = username;
+});
+
+controller.controller('clientCtrl', function($scope, $rootScope, $cookies, $location, clientService){
+	clientService.clientsInfo().then(function(data){
+		$rootScope.clients = data;
+	});
+	$scope.goDetail = function(){
+		var id = $(this)[0].data.id;
+		$cookies.put('page', "client_" + id);
+		$location.path('/detail').replace();
+	}
+	$scope.change = function(index){
+		$(".wchange").css("display","block");
+		$rootScope.nowClient = $rootScope.clients[index];
+		$(".changeName").val($rootScope.nowClient.name);
+		$(".changeStatu").val($rootScope.nowClient.statu);
+	}
+	$scope.delete = function(index){
+		$rootScope.clients.splice(index,1);
+	}
+	
+});
+
+$(".cancel").click(function(){
+	$(".wadd").css("display","none");
+	$(".wchange").css("display","none");
+});
+
+controller.controller('productCtrl', function($scope, $rootScope, $cookies, $location, productService){
+	productService.productsInfo().then(function(data){
+		$rootScope.products = data;
+	});
+	$scope.goDetail = function(){
+		var id = $(this)[0].data.id;
+		$cookies.put('page', "product_" + id);
+		$location.path('/detail').replace();
+	}
+	$scope.change = function(index){
+		$(".wchange").css("display","block");
+		$rootScope.nowProduct = $rootScope.products[index];
+		$(".changeName").val($rootScope.nowProduct.name);
+		$(".changeStatu").val($rootScope.nowProduct.statu);
+	}
+	$scope.delete = function(index){
+		$rootScope.products.splice(index,1);
+	}
+});
+
+controller.controller('detailCtrl', function($scope, detailService){
+	detailService.page().then(function(data){
+		if(data[0].phone){
+			$scope.page = {
+				id : "编号： " + data[0].id,
+				name : "用户名： " + data[0].name,
+				statu : "状态： " + data[0].statu,
+	            other : "电话： " + data[0].phone,
+	            amount : ""
+			}
+		}else{
+			$scope.page = {
+				id : "编号： " + data[0].id,
+				name : "商品名： " + data[0].name,
+				statu : "状态： " + data[0].statu,
+	            other : "产地： " + data[0].addr,
+	            amount : "数量： " + data[0].amount,
+			}
+		}
+		
+	});
+});
+
+controller.controller('waddCtrl', function($scope, $rootScope, detailService){
 
 });
 
-controller.controller('asideCtrl', function($scope){
-
+controller.controller('wchangeCtrl', function($scope, $rootScope, detailService){
+	$scope.change = function(){
+		$(".wadd").css("display","none");
+		$(".wchange").css("display","none");
+		console.log($rootScope.nowClient);
+		if ($rootScope.nowClient) {
+			$rootScope.nowClient.name = $(".changeName").val();
+			$rootScope.nowClient.statu = $(".changeStatu").val();
+			$rootScope.clients.splice($rootScope.nowClient.id - 1, 1, $rootScope.nowClient);
+			console.log($rootScope.clients);
+		}else if ($rootScope.nowProduct) {
+			$rootScope.nowProduct.name = $(".changeName").val();
+			$rootScope.nowProduct.statu = $(".changeStatu").val();
+			$rootScope.products.splice($rootScope.nowProduct.id - 1, 1, $rootScope.nowProduct);
+		}
+	}
+	
 });
 
-controller.controller('clientCtrl', function($scope){
-
-});
-
-controller.controller('productCtrl', function($scope){
-
-});
+controller.controller('waddCtrl', function($scope, $rootScope, $location, detailService){
+	$scope.add = function(){
+		var page = $location.path();
+		if (page.indexOf('lient') > 0) {
+			$rootScope.clients.push({
+				id : $rootScope.clients[$rootScope.clients.length - 1].id + 1,
+				name : $(".addName").val(),
+				statu : $(".addStatu").val()
+			}) 
+		}else{
+			$rootScope.products.push({
+				id : $rootScope.products[$rootScope.products.length - 1].id + 1,
+				name : $(".addName").val(),
+				amount : $(".addStatu").val()
+			})
+		}
+		$(".wadd").css("display", "none");
+	}
+})
